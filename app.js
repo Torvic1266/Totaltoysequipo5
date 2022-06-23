2ec3f8c2ade4c6574143be2cfa02861379f84bd4
torvic1266 <jlvictor28@hotmail.com>
2022-06-22 20:10:46 -0500

    se subio ejs

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 const express = require("express");
 const path=require("path");
 const app = express();
+app.set("view engine","ejs");
 
 const publicPath =path.resolve(__dirname,"./public");
 app.use(express.static(publicPath));
-app.listen(3600,()=> {
-    console.log("servidor corriendo en el puerto 3600");
+app.listen(3001,()=> {
+    console.log("servidor corriendo en el puerto 3001");
 });
 app.get("/",(req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/index.html"));
+    res.render("index.ejs");
       
 });
 
 
 app.get("/login",(req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+    res.render("login.ejs")
       
 });
 
 
 app.get("/productdetail",(req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/productdetail.html"))
+    res.render("productDetail.ejs")
 
       
 });
   
 app.get("/productcart",(req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/productcart.html"))
+    res.render("productcart.ejs")
       
 });
 app.get("/register",(req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+    res.render("register.ejs")
       
 });
 
