1940bc2469bd5ad8c4ddf46db05017646cf82ccd
08253565l <claudiacanom@hotmail.com>
2022-07-05 18:56:58 -0500

    cambios en el archivo

--- controllers/main.js
+++ controllers/main.js
@@ -1,2 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const menu = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/menu.json"), 'utf-8'));
+
+const main = {
+    index: (req,res)=>{
+        res.render("index",{menu});      
+    }, 
+    detalle:(req,res)=>{
+        let producto = menu.find(producto => producto.id === + req.params.id);
+        return res.render("detalleMenu",{producto});
+    }
+};
+
+
+module.exports = main;
