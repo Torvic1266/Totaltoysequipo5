e5b5cdd78a243b92e764673384d566a380b6dba4
harold <boteroharold63@gmail.com>
2022-09-27 19:37:42 -0500

    modelo-usuario

--- models/User.js
+++ models/User.js
@@ -1,60 +1,26 @@
 
-const fs = require('fs');
-
-const User = {
+module.exports = (sequelize, dataTypes) => {
+    let alias = "categorias";
+    let columnas = {
+      id : {
+          type: dataTypes.INTEGER, 
+          primaryKey: true,
+          autoIncrement: true,
+        },
+      nombre:{
+           type: dataTypes.STRING
+      },
+      descripcion:{
+        type: dataTypes.STRING
+      },
+        precio: dataTypes.FLOAT
+    };
+    let config = {
+        tableName : "categoria",
+        timestamps : false
+    }
     
-    fileName: './data/users.json',
-
-getData: function () {
-    return JSON.parse(fs.readFileSync(this.fileName,'utf-8'));
-},
-
-generateId: function () {
-    let allUsers = this.findAll();
-    let lastUser = allUsers.pop();
-    if (lastUser) {
-    return lastUser.id + 1;
-    }
-    return 1;
-},
-
-findAll: function () {
-    return this.getData();
-},
-
-
-findByPk: function (id) {
- let allUsers = this.findAll();
- let userFound = allUsers.find(oneUser => oneUser.id === id);
- return userFound;
-},
-
-findByField: function (field,text) {
-    let allUsers = this.findAll();
-    let userFound = allUsers.find(oneUser => oneUser[field] === text);
-    return userFound;
-   },
-
-    create:function (userData){
-        let allUsers = this.findAll();
-        let newUser = {
-            id: this.generateId(),
-            ...userData
-        }
-        allUsers.push(newUser);
-        fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null, ' '));
-        return newUser;
-    },
-
-    delete: function (id) {
-        let allUsers = this.findAll();
-        let finalUsers = allUsers.filter(oneUser => oneUser.id !== id);
-        allUsers.push(newUser);
-        fs.writeFileSync(this.fileName, JSON.stringify(finalUsers, null, ' '));
-        return true;
-    }
-
-}
-
-
-module.exports = User;
+    const categoria = sequelize.define(alias, columnas, config);
+    
+    return categoria;
+    } 
