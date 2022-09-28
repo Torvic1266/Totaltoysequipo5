5d46535d9173310d0f093802153450398c4fea7c
harold <boteroharold63@gmail.com>
2022-09-28 18:10:45 -0500

    hhhhn

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
   "development": {
     "username": "root",
-    "database": "database_development",
+    "database": "totaltoysDB",
     "host": "127.0.0.1",
     "dialect": "mysql"
     
