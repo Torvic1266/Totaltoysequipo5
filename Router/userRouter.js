1f05528f2d3685f5c4a7ee59c6360c71b7e7e807
maria <ericapolo10@hotmail.com>
2022-09-30 16:52:23 -0500

    agrego multer en el router

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,15 +1,28 @@
-const express = require ("express");
+const express = require("express");
 const router = express.Router();
 const usersController = require('../controllers/usersController');
-
+const multer = require("multer");
 const uploadFile = require('../middlewares/multerMiddleware');
 const validations = require('../middlewares/validateRegisterMiddleware');
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require("../middlewares/authMiddleware");
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/img/avatar')
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        let filename = file.originalname
+        cb(null, filename)
+    }
+})
 
-router.get('/register',guestMiddleware,usersController.register);
+const imgAvatar = multer({storage});
 
-router.post('/register',guestMiddleware,uploadFile.single('avatar'),validations,usersController.processRegister);
+
+router.get('/register', guestMiddleware, usersController.register);
+
+router.post('/register', guestMiddleware, imgAvatar.single('avatar'), validations, usersController.processRegister);
 
 router.get('/login', guestMiddleware, usersController.login);
 
