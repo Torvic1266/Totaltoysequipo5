2c9aebecc13288240a53bd6d8ada1cfe2dd23423
harold <boteroharold63@gmail.com>
2022-10-04 20:31:34 -0500

    modelos

---
+++ database/models/Categorias.js
@@ -0,0 +1,49 @@
+const { name } = require("ejs");
+
+module.exports = (sequelize, dataTypes) => {
+    const Productos = sequelize.define ("Productos",{
+
+        id:{
+            type: dataTypes.INTEGER, 
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name:{
+            type: dataTypes.STRING,
+            allowNull:false
+
+        },
+        description:{
+            type: dataTypes.TEXT,
+            allowNull:false
+        },
+        price:{
+            type: dataTypes.FLOAT,
+            allowNull:false
+        },
+        image:{
+            type: dataTypes.STRING,
+            allowNull:false
+        },
+        category_id:{
+            type: dataTypes.INTEGER,
+            allowNull:false,
+        },
+        is_active:
+        { type: dataTypes.INTEGER,
+         defaultValue:1
+        }
+        
+    },
+    {
+        timeStamps:false,
+        tableName:"productos",
+        underscored:true
+    });
+    
+    
+    
+    
+    
+    return productos;
+    } 
