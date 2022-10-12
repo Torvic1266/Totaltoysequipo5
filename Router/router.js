e597bfcf849c1e7f1d260387adc3ea1bbf92b4fb
chenaoh <claudiacanom@hotmail.com>
2022-10-12 18:12:28 -0500

    fix crear producto

--- Router/router.js
+++ Router/router.js
@@ -5,7 +5,7 @@ const routerProductos = require("./productRouter");
 const userRouter = require('./userRouter');
 
 router.get("/", menuController.index);
-//router.use("/productos", routerProductos);
+router.use("/productos", routerProductos);
 router.use("/usuario", userRouter);
 
 module.exports = router;
