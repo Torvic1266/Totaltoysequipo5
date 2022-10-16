8eb563af2224f3f30c298a8c39ee0af46103c43f
Mariaclara0310 <ericapolo10@hotmail.com>
2022-10-15 19:54:46 -0500

    Modelo usuario

---
+++ src/database/models/Usuario.js
@@ -0,0 +1,52 @@
+/**
+ * Import Sequelize.
+ */
+ const { Sequelize, DataTypes } = require("sequelize");
+
+ /**
+  * Import the Sequelize instance that you have exported
+  * in the config/database.js file.
+  */
+ const sequelize = require("../database/connection");
+ 
+ /**
+  * Define a model that can be managed by Sequelize.
+  */
+ const Usuario = sequelize.define(
+     "app_usuarios",
+     {
+         id: {
+             type: DataTypes.INTEGER,
+             autoIncrement: true,
+             allowNull: false,
+             primaryKey: true,
+         },
+         nombre: {
+             type: DataTypes.STRING,
+             allowNull: false,
+         },
+         descripcion: {
+             type: DataTypes.STRING,
+             allowNull: false,
+         },
+         edad: {
+             type: DataTypes.STRING,
+             allowNull: true,
+         },
+         nacionalidad: {
+             type: DataTypes.STRING,
+             allowNull: true,
+         },
+     },
+     {
+         /**
+          * Other model options go here
+          */
+     }
+ );
+ 
+ /**
+  * Export the model, so that it can be used in any
+  * page to execute CRUD operations on the app_posts table.
+  */
+ module.exports = User;
