703fc10159e92ccdabb56cbf9218fd43a25971e6
VictorGiraldoG <104600192+Torvic1266@users.noreply.github.com>
2022-10-15 19:20:15 -0500

    CRUD: lectura de productos (index)

--- controllers/main.js
+++ controllers/main.js
@@ -1,18 +1,30 @@
 const { response } = require("express");
 const express = require("express");
 const router = express.Router();
+
+const db = require("../database/connection");
+const Producto = require("../models/Producto")
+
+/*
 const fs = require('fs');
 const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+*/
 
 const main = {
     index: (req,res)=>{
-    res.render("index", {products}); 
-       
-    }, 
-    
-    
+        db.sync().then(() => {                    
+            Producto.findAll()
+            .then(function(productos){
+                res.render("index", {Productos: productos})
+            }).catch((error) => {
+                console.error('Error al listar productos: ', error);
+            });
+        }).catch((error) => {
+            console.error('Error en la conexión con la base de datos: ', error);
+        });
+    },
 }
 
 module.exports = main;
