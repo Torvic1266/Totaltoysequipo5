834b013569553fda525099713285c677de2e1f02
maria <ericapolo10@hotmail.com>
2022-10-18 16:27:29 -0500

    Arreglo la vista de editar

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,7 +6,7 @@ const { validationResult } = require("express-validator");
 
 
 const User = require("../models/Usuario");
-const db = require('../database/models');
+const db = require('../database/connection');
 
 
 
