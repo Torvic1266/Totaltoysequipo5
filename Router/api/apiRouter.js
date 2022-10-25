f199491a06ac1c94dd12209b5aa7a90c663969a0
Mariaclara0310 <ericapolo10@hotmail.com>
2022-10-25 11:24:47 -0500

    se modifica api Router

--- Router/api/apiRouter.js
+++ Router/api/apiRouter.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const ApiProductController = require('../../controllers/api/apiProductControllers');
-router.get ('/User/:id', ApiProductController.Totaltoys5);
-router.get('/Totaltoys', ApiProductController.getAllTOTALTOY);
+router.get ('/juguete/:id', ApiProductController.Totaltoys5);
+router.get('/juguetes', ApiProductController.getAllTOTALTOY);
 
-const ApiUsuarioController = require('../../src/database/models');
-router.get('/User/:id', ApiUsuarioController.getAllregister);
-router.get('/loguin',ApiUsuarioController.getAllTOTALTOY);
+const ApiUsuarioController = require('../../controllers/api/apiUsuarioController');
+router.get('/usuario/:id', ApiUsuarioController.getUserById);
+router.get('/usuarios',ApiUsuarioController.getAllUsers);
 
 module.exports = router;
