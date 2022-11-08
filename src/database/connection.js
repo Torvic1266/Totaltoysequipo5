9743f6b32f21beec8715c8718ad25ba3691963a4
Mariaclara0310 <ericapolo10@hotmail.com>
2022-11-08 14:11:16 -0500

    base de datos en el servidor

--- src/database/connection.js
+++ src/database/connection.js
@@ -7,11 +7,10 @@ const Sequelize = require("sequelize");
  * Create a Sequelize instance. This can be done by passing
  * the connection parameters separately to the Sequelize constructor.
  */
-const sequelize = new Sequelize("totaltoys", "root", "", {
-    host: "127.0.0.1",
+ const sequelize = new Sequelize("sql9552987", "sql9552987", "jNwkJLz7LL", {
+    host: "sql9.freemysqlhosting.net",
     dialect: "mysql"
 });
-
 /**
  * Export the Sequelize instance. This instance can now be 
  * used in the app.js file to authenticate and establish a database connection.
